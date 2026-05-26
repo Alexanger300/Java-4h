cd45bb9dbe8cfeee345cdcc2afd70bd6795f89a7
Dylan-MOUTY <dylanmouty77@gmail.com>
2026-05-26 15:06:07 +0200

    correction erreur majuscule

--- gameRound.java
+++ gameRound.java
@@ -1,4 +1,4 @@
-public static int gameRound(Int playerChoice, int npcChoice) {
+public static int gameRound(int playerChoice, int npcChoice) {
     int result;
     System.out.println("Votre choix est "+playerSelection(playerChoice)+"Le choix du bot est" +playerSelection(npcChoice));
     if (playerChoice==1&&npcChoice==3||playerChoice==2&&npcChoice==1||playerChoice==3&&npcChoice==2){
